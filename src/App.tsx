bd4331ac5dbd83b800160baa56d8570060a4dab0
Florin Badita <baditaflorin@gmail.com>
2026-05-19 00:02:37 +0300

    feat: mount MeshBeacon next to InviteShareButton in non-MeshShell holdouts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Metronome, type PatternId } from "./features/metronome/Metronome";
 import { SettingsDrawer } from "./features/settings/SettingsDrawer";
 import { appConfig } from "./shared/config";
-import { InviteShareButton } from "@baditaflorin/mesh-common";
+import { InviteShareButton, MeshBeacon } from "@baditaflorin/mesh-common";
 
 const STORAGE = {
   room: `${appConfig.storagePrefix}:room`,
@@ -41,6 +41,8 @@ export function App() {
       <Metronome roomId={roomId} pattern={pattern} bpm={bpm} haptic={haptic} />
 
       <InviteShareButton appName={appConfig.appName} roomId={roomId} />
+      <MeshBeacon app={appConfig.appName} room={roomId} />
+
       <button
         type="button"
         className="settings-fab"
