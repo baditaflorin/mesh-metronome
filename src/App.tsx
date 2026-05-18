3464693aefdfc33b730d664674d05e6dec55b446
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-18 19:58:25 +0300

    feat: add InviteShareButton retrofit + mesh-common styles bridge + vitest dedupe

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Metronome, type PatternId } from "./features/metronome/Metronome";
 import { SettingsDrawer } from "./features/settings/SettingsDrawer";
 import { appConfig } from "./shared/config";
+import { InviteShareButton } from "@baditaflorin/mesh-common";
 
 const STORAGE = {
   room: `${appConfig.storagePrefix}:room`,
@@ -39,6 +40,7 @@ export function App() {
     <div className="app-root">
       <Metronome roomId={roomId} pattern={pattern} bpm={bpm} haptic={haptic} />
 
+      <InviteShareButton appName={appConfig.appName} roomId={roomId} />
       <button
         type="button"
         className="settings-fab"
